cc4d22377f6019835d9da94e8fbc655cb221d8be
HwangHyeonSu <hwanghs111@naver.com>
2022-01-31 05:47:04 +0900

    home UI수정, activity 연결,expandable layout

--- app/src/main/java/com/mummoom/md/ui/main/community/CommunityActivity.kt
+++ app/src/main/java/com/mummoom/md/ui/main/community/CommunityActivity.kt
@@ -3,9 +3,6 @@ package com.mummoom.md.ui.main.community
 import com.mummoom.md.databinding.ActivityCommunityBinding
 import com.mummoom.md.ui.BaseActivity
 
-
-
-
 class CommunityActivity : BaseActivity<ActivityCommunityBinding>(ActivityCommunityBinding::inflate) {
 
     override fun initAfterBinding() {
