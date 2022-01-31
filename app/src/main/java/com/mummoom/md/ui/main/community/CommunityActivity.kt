fcab9bf11b5cc08ca8f1532ba31c17a542ed9d23
HwangHyeonSu <hwanghs111@naver.com>
2022-01-31 13:45:36 +0900

    다시 PR

--- app/src/main/java/com/mummoom/md/ui/main/community/CommunityActivity.kt
+++ app/src/main/java/com/mummoom/md/ui/main/community/CommunityActivity.kt
@@ -3,6 +3,7 @@ package com.mummoom.md.ui.main.community
 import com.mummoom.md.databinding.ActivityCommunityBinding
 import com.mummoom.md.ui.BaseActivity
 
+
 class CommunityActivity : BaseActivity<ActivityCommunityBinding>(ActivityCommunityBinding::inflate) {
 
     override fun initAfterBinding() {
