d103ccd3a3bef4af38586b648522699c0e33fd4e
KangGeonyoung <kky6335@gmail.com>
2022-02-18 17:50:00 +0900

    Modified the MyActiveActivity's UI.

--- app/src/main/java/com/mummoom/md/ui/main/community/CommunityFragment.kt
+++ app/src/main/java/com/mummoom/md/ui/main/community/CommunityFragment.kt
@@ -4,8 +4,12 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Parcelable
 import android.util.Log
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.GridLayout
 import androidx.recyclerview.widget.GridLayoutManager
+import com.mummoom.md.R
 import com.mummoom.md.data.Post.Post
 import com.mummoom.md.data.remote.Post.GetPostsView
 import com.mummoom.md.data.remote.Post.PostService
