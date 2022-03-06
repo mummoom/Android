81e89255c3f0383f4d5c79e77d445961a2c2ee32
HwangHyeonSu <hwanghs111@naver.com>
2022-03-06 22:09:16 +0900

    신고 다이얼로그 라디오버튼으로 변경

--- app/src/main/java/com/mummoom/md/ui/main/community/ReportDialog.kt
+++ app/src/main/java/com/mummoom/md/ui/main/community/ReportDialog.kt
@@ -25,63 +25,18 @@ class ReportDialog(context : Context) {
         val reasonEt = dialog.findViewById<EditText>(R.id.reportDialog_reason_et)
         val doneBtn = dialog.findViewById<TextView>(R.id.reportDialog_doneBtn_btn)
         val cancelBtn = dialog.findViewById<TextView>(R.id.reportDialog_cancelBtn_btn)
+        val abuseBtn = dialog.findViewById<RadioButton>(R.id.reportDialog_abuse_rb)
+        val promotionBtn = dialog.findViewById<RadioButton>(R.id.reportDialog_promotion_rb)
+        val obscenityBtn = dialog.findViewById<RadioButton>(R.id.reportDialog_obscenity_rb)
+        val repeatBtn = dialog.findViewById<RadioButton>(R.id.reportDialog_repeat_rb)
+        val otherBtn = dialog.findViewById<RadioButton>(R.id.reportDialog_other_rb)
 
-        val abuseOnBtn = dialog.findViewById<ImageView>(R.id.reportDialog_abuse_iv_on)
-        val abuseOffBtn = dialog.findViewById<ImageView>(R.id.reportDialog_abuse_iv_off)
-        val promotionOnBtn = dialog.findViewById<ImageView>(R.id.reportDialog_promotion_iv_on)
-        val promotionOffBtn = dialog.findViewById<ImageView>(R.id.reportDialog_promotion_iv_off)
-        val obscenityOnBtn = dialog.findViewById<ImageView>(R.id.reportDialog_obscenity_iv_on)
-        val obscenityOffBtn = dialog.findViewById<ImageView>(R.id.reportDialog_obscenity_iv_off)
-        val repeatOnBtn = dialog.findViewById<ImageView>(R.id.reportDialog_repeat_iv_on)
-        val repeatOffBtn = dialog.findViewById<ImageView>(R.id.reportDialog_repeat_iv_off)
-        val otherOnBtn = dialog.findViewById<ImageView>(R.id.reportDialog_other_iv_on)
-        val otherOffBtn = dialog.findViewById<ImageView>(R.id.reportDialog_other_iv_off)
 
-        //욕설
-        abuseOnBtn.setOnClickListener {
-            abuseOffBtn.visibility= View.INVISIBLE
-            abuseOnBtn.visibility=View.VISIBLE
-        }
-        abuseOffBtn.setOnClickListener {
-            abuseOffBtn.visibility=View.VISIBLE
-            abuseOnBtn.visibility= View.INVISIBLE
-        }
-        //홍보성
-        promotionOnBtn.setOnClickListener {
-            promotionOnBtn.visibility=View.VISIBLE
-            promotionOffBtn.visibility= View.INVISIBLE
-        }
-        promotionOffBtn.setOnClickListener {
-            promotionOffBtn.visibility=View.VISIBLE
-            promotionOnBtn.visibility= View.INVISIBLE
-        }
-        //음란성
-        obscenityOnBtn.setOnClickListener {
-            obscenityOnBtn.visibility=View.VISIBLE
-            obscenityOffBtn.visibility= View.INVISIBLE
-        }
-        obscenityOffBtn.setOnClickListener {
-            obscenityOnBtn.visibility=View.VISIBLE
-            obscenityOffBtn.visibility= View.INVISIBLE
-        }
-        //도배
-        repeatOnBtn.setOnClickListener {
-            repeatOnBtn.visibility=View.VISIBLE
-            repeatOffBtn.visibility= View.INVISIBLE
-        }
-        repeatOffBtn.setOnClickListener {
-            repeatOnBtn.visibility=View.VISIBLE
-            repeatOffBtn.visibility= View.INVISIBLE
-        }
-        //기타
-        otherOnBtn.setOnClickListener {
-            otherOnBtn.visibility=View.VISIBLE
-            otherOffBtn.visibility= View.INVISIBLE
-        }
-        otherOffBtn.setOnClickListener {
-            otherOnBtn.visibility=View.VISIBLE
-            otherOffBtn.visibility= View.INVISIBLE
-        }
+        abuseBtn.setOnClickListener{}
+        promotionBtn.setOnClickListener{}
+        obscenityBtn.setOnClickListener{}
+        repeatBtn.setOnClickListener{}
+        otherBtn.setOnClickListener{}
 
         // 확인 버튼 눌렀을 때
         doneBtn.setOnClickListener {
